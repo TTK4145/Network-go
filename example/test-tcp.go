6563ea84cd0539f92750630ac30329564132ad89
itk-d674 <kyrreerl@itk-d651>
2015-02-24 12:31:56 +0100

    fixed error

--- example/test-tcp.go
+++ example/test-tcp.go
@@ -4,19 +4,20 @@ import (
 	"fmt"
 	"../tcp"
 	"time"
+	"strconv"
 )
 
 
 func main (){
 	fmt.Println("Starting test-tcp.go")
-	lport := 20234
+	lport := 20233
 	rchan := make (chan tcp.Tcp_message)
 	schan := make (chan tcp.Tcp_message)
 	tcp.Tcp_init(lport, schan, rchan)
 	
 	go func(ch chan tcp.Tcp_message){
 		id := 0
-		msg := {Raddr: "129.241.187.XXX:20233", Data := strconv.Itoa(id), Length = 32}
+		msg := tcp.Tcp_message{Raddr: "129.241.187.118:20234", Data: strconv.Itoa(id), Length: 32}
 
 		for {
 			msg.Data = strconv.Itoa(id)
