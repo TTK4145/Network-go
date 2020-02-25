dd0259ae8a9c8e82a08c10d64029a8afbf46ca1d
Anders Petersen <klasbo@gmail.com>
2020-02-25 14:42:07 +0100

    Increase peer-lost timeout
    From 3 consecutive lost messages to 33. This decreases sensitivity to packet loss, and will more reliably trigger only on complete peer loss.

--- network/peers/peers.go
+++ network/peers/peers.go
@@ -15,7 +15,7 @@ type PeerUpdate struct {
 }
 
 const interval = 15 * time.Millisecond
-const timeout = 50 * time.Millisecond
+const timeout = 500 * time.Millisecond
 
 func Transmitter(port int, id string, transmitEnable <-chan bool) {
 
