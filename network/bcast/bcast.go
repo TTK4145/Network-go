5c3cb580ea72c68663b82c3c007303da477108fc
Anders Petersen <klasbo@gmail.com>
2021-04-06 15:49:23 +0200

    Bugfix: Recursively test types

--- network/bcast/bcast.go
+++ network/bcast/bcast.go
@@ -88,7 +88,7 @@ func checkArgs(chans ...interface{}) {
 		// Must be a channel
 		if reflect.ValueOf(ch).Kind() != reflect.Chan {
 			panic(fmt.Sprintf(
-				"Argument must be a channel, got '%s' instead (arg#%d)",
+				"Argument must be a channel, got '%s' instead (arg# %d)",
 				reflect.TypeOf(ch).String(), i+1))
 		}
 
@@ -98,24 +98,37 @@ func checkArgs(chans ...interface{}) {
 		for j, e := range elemTypes {
 			if e == elemType {
 				panic(fmt.Sprintf(
-					"All channels must have mutually different element types, arg#%d and arg#%d both have element type '%s'",
+					"All channels must have mutually different element types, arg# %d and arg# %d both have element type '%s'",
 					j+1, i+1, e.String()))
 			}
 		}
 		elemTypes[i] = elemType
 
 		// Element type must be encodable with JSON
-		switch elemType.Kind() {
-		case reflect.Complex64, reflect.Complex128, reflect.Chan, reflect.Func, reflect.UnsafePointer:
+		checkTypeRecursive(elemType, []int{i+1})
+
+	}
+}
+
+
+func checkTypeRecursive(val reflect.Type, offsets []int){
+	switch val.Kind() {
+	case reflect.Complex64, reflect.Complex128, reflect.Chan, reflect.Func, reflect.UnsafePointer:
+		panic(fmt.Sprintf(
+			"Channel element type must be supported by JSON, got '%s' instead (nested arg# %v)",
+			val.String(), offsets))
+	case reflect.Map:
+		if val.Key().Kind() != reflect.String {
 			panic(fmt.Sprintf(
-				"Channel element type must be supported by JSON, got '%s' instead (arg#%d)",
-				elemType.String(), i+1))
-		case reflect.Map:
-			if elemType.Key().Kind() != reflect.String {
-				panic(fmt.Sprintf(
-					"Channel element type must be supported by JSON, got '%s' instead (map keys must be 'string') (arg#%d)",
-					elemType.String(), i+1))
-			}
+				"Channel element type must be supported by JSON, got '%s' instead (map keys must be 'string') (nested arg# %v)",
+				val.String(), offsets))
+		}
+		checkTypeRecursive(val.Elem(), offsets)
+	case reflect.Array, reflect.Ptr, reflect.Slice:
+		checkTypeRecursive(val.Elem(), offsets)
+	case reflect.Struct:
+		for idx := 0; idx < val.NumField(); idx++ {
+			checkTypeRecursive(val.Field(idx).Type, append(offsets, idx+1))
 		}
 	}
 }
