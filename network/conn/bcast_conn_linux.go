a0e65a624e86f695c7624b2f8be41dcd6ca8efea
Anders Petersen <klasbo@gmail.com>
2022-01-28 22:33:24 +0100

    Bugfix: Print error values

--- network/conn/bcast_conn_linux.go
+++ network/conn/bcast_conn_linux.go
@@ -11,17 +11,17 @@ import (
 
 func DialBroadcastUDP(port int) net.PacketConn {
 	s, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_DGRAM, syscall.IPPROTO_UDP)
-	if err != nil { fmt.Printf("Error: Socket: %#+v\n") }
+	if err != nil { fmt.Println("Error: Socket:", err) }
 	syscall.SetsockoptInt(s, syscall.SOL_SOCKET, syscall.SO_REUSEADDR, 1)
-	if err != nil { fmt.Printf("Error: SetSockOpt REUSEADDR: %#+v\n") }
+	if err != nil { fmt.Println("Error: SetSockOpt REUSEADDR:", err) }
 	syscall.SetsockoptInt(s, syscall.SOL_SOCKET, syscall.SO_BROADCAST, 1)
-	if err != nil { fmt.Printf("Error: SetSockOpt BROADCAST:  %#+v\n") }
+	if err != nil { fmt.Println("Error: SetSockOpt BROADCAST:", err) }
 	syscall.Bind(s, &syscall.SockaddrInet4{Port: port})
-	if err != nil { fmt.Printf("Error: Bind:  %#+v\n") }
+	if err != nil { fmt.Println("Error: Bind:", err) }
 
 	f := os.NewFile(uintptr(s), "")
 	conn, err := net.FilePacketConn(f)
-	if err != nil { fmt.Printf("Error: FilePacketConn: %#+v\n") }
+	if err != nil { fmt.Println("Error: FilePacketConn:", err) }
 	f.Close()
 
 	return conn
