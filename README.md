fb8a943efb0626b8a6dcdf88d6d84511cf4d151c
Anders Petersen <klasbo@gmail.com>
2022-01-28 22:34:30 +0100

    Remove Windows C compiler dependency by using ListenConfig

--- README.md
+++ README.md
@@ -14,24 +14,13 @@ Where `./Network-go` is the relative path to this folder, after you have downloa
 Features
 --------
 
-Channel-in/channel-out pairs of (almost) any custom or built-in datatype can be supplied to a pair of transmitter/receiver functions. Data sent to the transmitter function is automatically serialized and broadcasted on the specified port. Any messages received on the receiver's port are deserialized (as long as they match any of the receiver's supplied channel datatypes) and sent on the corresponding channel. See [bcast.Transmitter and bcast.Receiver](network/bcast/bcast.go).
+Channel-in/channel-out pairs of (almost) any custom or built-in data type can be supplied to a pair of transmitter/receiver functions. Data sent to the transmitter function is automatically serialized and broadcast on the specified port. Any messages received on the receiver's port are de-serialized (as long as they match any of the receiver's supplied channel data types) and sent on the corresponding channel. See [bcast.Transmitter and bcast.Receiver](network/bcast/bcast.go).
 
-Peers on the local network can be detected by supplying your own ID to a transmitter and receiving peer updates (new, current and lost peers) from the receiver. See [peers.Transmitter and peers.Receiver](network/peers/peers.go).
+Peers on the local network can be detected by supplying your own ID to a transmitter and receiving peer updates (new, current, and lost peers) from the receiver. See [peers.Transmitter and peers.Receiver](network/peers/peers.go).
 
 Finding your own local IP address can be done with the [LocalIP](network/localip/localip.go) convenience function, but only when you are connected to the internet.
 
-For Windows users
------------------
 
-In order to use this module on Windows, you must install a C compiler. I recommend [TDM-GCC](http://tdm-gcc.tdragon.net/download), which is a redistribution of GCC and MinGW.
-
-You may need to add the C toolchain binaries to your Environment Path manually. If you can run `gcc` from either cmd or Powershell, it is already working. If not, open the environment variables dialog the long way, or the short way with `Win+R` and typing `rundll32 sysdm.cpl,EditEnvironmentVariables`. Edit the PATH variable by adding `;C:\TDM-GCC-64\bin` (or whatever your chosen install directory was) at the end of the semi-colon-delimited list. You may also need to restart or log off & back on in order for the changes to take effect. 
-
-The error return values on Windows just display numbers instead of descriptive text. See [here for descriptions of the error codes](https://msdn.microsoft.com/en-us/library/windows/desktop/ms740668(v=vs.85).aspx)
-
-----
-
-If you are wondering why Windows is such a hassle, read the comments in [network/conn/bcast_conn_windows.go](network/conn/bcast_conn_windows.go)
 
 
 
--- network/conn/bcast_conn_windows.go
+++ network/conn/bcast_conn_windows.go
@@ -17,183 +17,52 @@ Alternative 2: Syscalls
 Instead of using the net package, we use syscall and just do it the proper way, and turn the file descriptor / handle into a 
 "file connection". This works fine (as in "it works", not "it's fine" - because it is stupid) on posix, but does not work on 
 Windows because reasons, where "reasons" are *it literally just says TODO in the standard library*:
-https://github.com/golang/go/blob/dfb0e4f6c744eb9bf629658bf7da313b2d1518e1/src/net/file_windows.go
+https://github.com/golang/go/blob/ef0b09c526d78de23186522d50ff93ad657014c0/src/net/file_windows.go
 
 Alternative 3: Golang shim of Windows API
 The next option is to use sys/x/windows to interface with the Windows API directly:
 https://godoc.org/golang.org/x/sys/windows
 Which would work fine - and does work fine for most of the API - until again...  It's just not implemented yet:
 https://github.com/golang/sys/blob/37707fdb30a5b38865cfb95e5aab41707daec7fd/windows/syscall_windows.go#L973-L980
+Update 2022: This was added in December 2019: https://github.com/golang/sys/commit/6d18c012aee9febd81bbf9806760c8c4480e870d
+But x/sys/windows is still an external package, which means a separate "go get" call and a flimsy dependency on GOROOT/GOPATH
 
 Alternative 4: WSASockets from the Windows API
 WSA Sockets are like normal sockets, but with more options and more parameters. I was not able to make them work though...
+These are part of internal/syscall/windows, but go does not let users import internal packages
 
 Alternative 5: Just write C code
-This is what is done below. All the socket code is written in C, and called from Go by using CGO, which therefore requires 
+This is what done up to 2022. All the socket code was written in C, and called from Go by using CGO, which therefore required 
 a C compiler.
+
+Alternative 6: Use ListenConfig, added in Go 1.11 (August 2018)
+This is what is done below. A ListenConfig takes a callback that takes a callback that can operate on the file descriptor, 
+which is then called by the ListenPacket function when setting up the socket. Absolutely not intuitive, and definitely not 
+"the standard way", but it works.
+This eliminates the need for a C compiler
 */
 
 
 
-/*
-#include<stdio.h>
-#include<winsock2.h>
-
-
-SOCKET cBcastSocket(u_short port){
-    SOCKET s;
-
-    if((s = socket(AF_INET , SOCK_DGRAM , 0 )) == INVALID_SOCKET){
-        printf("Could not create socket: %d\n" , WSAGetLastError());
-        exit(EXIT_FAILURE);
-    }
-
-    int opt = 1;
-    int optlen = sizeof(opt);
-    if(setsockopt(s, SOL_SOCKET, SO_BROADCAST, (char*)&opt, optlen) == SOCKET_ERROR){
-        printf("SO_BROADCAST failed with error code : %d\n" , WSAGetLastError());
-        exit(EXIT_FAILURE);
-    }
-    if(setsockopt(s, SOL_SOCKET, SO_REUSEADDR, (char*)&opt, optlen) == SOCKET_ERROR){
-        printf("SO_REUSEADDR failed with error code : %d\n" , WSAGetLastError());
-        exit(EXIT_FAILURE);
-    }
-
-    struct sockaddr_in a;
-    a.sin_family = AF_INET;
-    a.sin_addr.s_addr = INADDR_ANY;
-    a.sin_port = htons(port);
-
-    if(bind(s, (struct sockaddr*)&a, sizeof(a)) == SOCKET_ERROR){
-        printf("Bind failed with error code : %d\n" , WSAGetLastError());
-        exit(EXIT_FAILURE);
-    }
-
-    return s;
-}
-
-int cSendTo(SOCKET s, char* ip, u_short port, char* buf, int buflen){
-    struct sockaddr_in a;
-    a.sin_family = AF_INET;
-    a.sin_addr.s_addr = inet_addr(ip);
-    a.sin_port = htons(port);
-    return sendto(s, buf, buflen, 0, (struct sockaddr*)&a, sizeof(struct sockaddr));
-}
-
-int cRecvFrom(SOCKET s, char* buf, int buflen, char* addr){
-    struct sockaddr_in* a = (struct sockaddr_in*)addr;
-    int a_len = sizeof(struct sockaddr);
-    return recvfrom(s, buf, buflen, 0, (struct sockaddr *)a, &a_len);
-}
-
-int cClose(SOCKET s){
-    return closesocket(s);
-}
-
-int cLocalAddr(SOCKET s, char* addr){
-    struct sockaddr_in* a = (struct sockaddr_in*)addr;
-    int a_len = sizeof(struct sockaddr);
-    return getsockname(s, (struct sockaddr *)a, &a_len);
-}
-
-int cSetReadDeadline(SOCKET s, int timeout_ms){
-    return setsockopt(s, SOL_SOCKET, SO_RCVTIMEO, (char*)&timeout_ms, sizeof(int));
-}
-
-int cSetWriteDeadline(SOCKET s, int timeout_ms){
-    return setsockopt(s, SOL_SOCKET, SO_SNDTIMEO, (char*)&timeout_ms, sizeof(int));
-}
-
- #cgo LDFLAGS: -lws2_32
-*/
-import "C"
 import (
-	"errors"
+    "context"
 	"fmt"
 	"net"
-	"time"
-	"unsafe"
+	"syscall"
 )
 
-type WindowsBroadcastConn struct {
-	Sock C.SOCKET
-}
-
-func (f WindowsBroadcastConn) ReadFrom(b []byte) (n int, addr net.Addr, err error) {
-	var addrbuf [16]byte
-	r := int(C.cRecvFrom(f.Sock, (*C.char)(unsafe.Pointer(&b[0])), C.int(len(b)), (*C.char)(unsafe.Pointer(&addrbuf[0]))))
-	if r == C.SOCKET_ERROR {
-		return 0, nil, errors.New(fmt.Sprintf("recvfrom() failed with error code %d", C.WSAGetLastError()))
-	} else {
-		addr := net.UDPAddr{IP: addrbuf[4:8], Port: int(C.ntohs(C.u_short(addrbuf[2]<<8) | C.u_short(addrbuf[3])))}
-		return r, &addr, nil
-	}
-}
-
-func (f WindowsBroadcastConn) WriteTo(b []byte, addr net.Addr) (n int, err error) {
-	r := int(C.cSendTo(f.Sock, C.CString(addr.(*net.UDPAddr).IP.String()), C.u_short(addr.(*net.UDPAddr).Port), (*C.char)(unsafe.Pointer(&b[0])), C.int(len(b))))
-	if r == C.SOCKET_ERROR {
-		return 0, errors.New(fmt.Sprintf("sendto() failed with error code %d", C.WSAGetLastError()))
-	} else {
-		return r, nil
-	}
-}
-
-func (f WindowsBroadcastConn) Close() error {
-	r := C.cClose(f.Sock)
-	if r == 0 {
-		return nil
-	} else {
-		return errors.New(fmt.Sprintf("closesocket() failed with error code %d", C.WSAGetLastError()))
-	}
-}
-
-func (f WindowsBroadcastConn) LocalAddr() net.Addr {
-	var addrbuf [16]byte
-	r := int(C.cLocalAddr(f.Sock, (*C.char)(unsafe.Pointer(&addrbuf[0]))))
-	if r == C.SOCKET_ERROR {
-		return nil
-	} else {
-		addr := net.UDPAddr{IP: addrbuf[4:8], Port: int(C.ntohs(C.u_short(addrbuf[2]<<8) | C.u_short(addrbuf[3])))}
-		return &addr
-	}
-}
-
-func (f WindowsBroadcastConn) SetDeadline(t time.Time) error {
-	e := f.SetReadDeadline(t)
-	if e != nil {
-		return e
-	}
-	e = f.SetWriteDeadline(t)
-	return e
-}
-
-func (f WindowsBroadcastConn) SetReadDeadline(t time.Time) error {
-	timeout_ms := int64(t.Sub(time.Now())) / 1000000
-	r := -1
-	if timeout_ms > 0 {
-		r = int(C.cSetReadDeadline(f.Sock, C.int(timeout_ms)))
-	}
-	if r == 0 {
-		return nil
-	} else {
-		return errors.New(fmt.Sprintf("setsockopt() failed with error code %d", C.WSAGetLastError()))
-	}
-}
-
-func (f WindowsBroadcastConn) SetWriteDeadline(t time.Time) error {
-	timeout_ms := int64(t.Sub(time.Now())) / 1000000
-	r := -1
-	if timeout_ms > 0 {
-		r = int(C.cSetWriteDeadline(f.Sock, C.int(timeout_ms)))
-	}
-	if r == 0 {
-		return nil
-	} else {
-		return errors.New(fmt.Sprintf("setsockopt() failed with error code %d", C.WSAGetLastError()))
-	}
-}
-
 func DialBroadcastUDP(port int) net.PacketConn {
-	return WindowsBroadcastConn{C.cBcastSocket(C.u_short(port))}
+    config := &net.ListenConfig{Control: 
+        func (network, address string, conn syscall.RawConn) error {
+            return conn.Control(func(descriptor uintptr) {
+                syscall.SetsockoptInt(syscall.Handle(descriptor), syscall.SOL_SOCKET, syscall.SO_REUSEADDR, 1)
+                syscall.SetsockoptInt(syscall.Handle(descriptor), syscall.SOL_SOCKET, syscall.SO_BROADCAST, 1)
+            })
+        },
+    }
+
+	conn, err := config.ListenPacket(context.Background(), "udp4", fmt.Sprintf(":%d", port)) 
+    fmt.Println(err)
+
+	return conn
 }
